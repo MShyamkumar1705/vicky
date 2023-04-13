a41e2ffc45d9f228f53fa0af3e6c21237b87913f
Shyam <samdhruv05@gmail.com>
2023-04-13 15:31:11 +0530

    commit 1

---
+++ src/test/java/demo/Demo.java
@@ -0,0 +1,17 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Demo {
+	@Test
+	public void test()
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		
+	}
+}
---
+++ src/test/java/demo/Example1.java
@@ -0,0 +1,23 @@
+package demo;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		int n=123;
+//		int lastDigit=n%10;
+//		int firstDigit=n;
+//		while(firstDigit>10) {
+//			firstDigit=firstDigit/10;
+//		}
+//		System.out.println(lastDigit+ " " +firstDigit);
+		
+		int sum=0;
+		while(n!=0) {
+			int d=n%10;
+			sum=sum*10+d;
+			n=n/10;
+		}
+		System.out.println(sum);
+	}
+
+}
